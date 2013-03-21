e882c8b1321b5b4aca5ab7da111dede36e66f652
Matthias Linder <matthias@matthiaslinder.com>
2013-03-21 13:11:25 +0100

    Updated comments

--- modules/py.py
+++ modules/py.py
@@ -4,6 +4,7 @@ import web
 from numpy import *
 from numpy.fft import *
 
+# Note: This service is potentially _VERY_ dangerous and should only be used in trusted locations
 def py(phenny, input): 
    """Call a webservice."""
    text = input.group(2)
